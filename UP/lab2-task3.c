93a806c0e19c73a0de5de8c5801f3c35434df6a2
Araashi <122167794+arashi-ez@users.noreply.github.com>
2023-02-23 15:14:01 +0500

    a-branch-which-asks-to-input-user-a-content
    a branch which asks to input user a content. out of main branch, which finds the string content due to user content

--- UP/lab2-task3.c
+++ UP/lab2-task3.c
@@ -7,27 +7,29 @@
 
 int main()
 {
-	char st[] = "INHA University in Tashkent";
-	char ch = 'U';
-	char* dest;
+	char st[100];
+    char* dest;
+    //this part of code asks to input a sentence which will represent in future usage
+    printf("Type your content to seek strings sequences: ");
+    gets(st);
+    
+    //first seek 
+	char ch = 'e';
+    
     printf("Content: %s\n",st);
 	dest = strrchr(st, ch);
+	printf("String after last %c is : %s \n", ch, dest);
 
-	printf("String after last %c is : %s \n",
-		ch, dest);
-
-	char ch2 = 'T';
-
+    //second seek
+    char ch2 = 't';
     dest = strrchr(st, ch2);
-
-	printf("String after last %c is : %s \n",
-		ch2, dest);
-
-
-    char ch3 = 'k';
+	printf("String after last %c is : %s \n", ch2, dest);
+    
+    //third seek
+    char ch3 = 'a';
     dest = strrchr(st, ch3);
+    printf("String after last %c is : %s ",	ch3, dest);
+	
+    return 0;
 
-    printf("String after last %c is : %s ",
-		ch3, dest);
-	return 0;
 }
