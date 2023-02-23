e71a3074e02ec64206f4110b644b485d4f5cb399
Araashi <122167794+arashi-ez@users.noreply.github.com>
2023-02-23 14:46:43 +0500

    Task2 finished

--- UP/lab2-task3.c
+++ UP/lab2-task3.c
@@ -1,41 +1,33 @@
-// C code to demonstrate the working of
-// strrchr()
-
+/*
+    Name:Ochilov Abdullajon
+    ID: U2010197
+*/
 #include <stdio.h>
 #include <string.h>
 
-// Driver function
 int main()
 {
-
-	// initializing variables
-	char st[] = "GeeksforGeeks";
-	char ch = 'G';
-	char* val;
-
-	// Use of strrchr()
-	// returns "ks"
-	val = strrchr(st, ch);
+	char st[] = "INHA University in Tashkent";
+	char ch = 'U';
+	char* dest;
+    printf("Content: %s\n",st);
+	dest = strrchr(st, ch);
 
 	printf("String after last %c is : %s \n",
-		ch, val);
+		ch, dest);
 
-	char ch2 = 'e';
-   
-	// Use of strrchr()
-	// returns null
-	// test for null
-	 val = strrchr(st, ch2);
+	char ch2 = 'T';
+
+    dest = strrchr(st, ch2);
 
 	printf("String after last %c is : %s \n",
-		ch2, val);
+		ch2, dest);
 
 
     char ch3 = 'k';
-    val = strrchr(st, ch3);
+    dest = strrchr(st, ch3);
+
     printf("String after last %c is : %s ",
-		ch3, val);
-
-
-	return (0);
+		ch3, dest);
+	return 0;
 }
